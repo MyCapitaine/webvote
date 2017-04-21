a642af246c81ea0b0bd3155c2ca01b4ca81dd6ef
lzs105 <578776370@qq.com>
2017-04-21 15:33:18 +0800

    information vote

---
+++ src/main/java/com/example/dao/LoginRecordDao.java
@@ -0,0 +1,20 @@
+package com.example.dao;
+
+import com.example.entity.LoginRecord;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+
+/**
+ * Created by hasee on 2017/4/19.
+ */
+public interface LoginRecordDao extends JpaRepository<LoginRecord, Integer> {
+    @Query(" from LoginRecord lr where lr.userId=?1 ")
+    Page findByUserId(int id, Pageable page);
+
+
+    @Query(" from LoginRecord lr where lr.id=:id ")
+    Page findCommentById(int id, Pageable page);
+}
