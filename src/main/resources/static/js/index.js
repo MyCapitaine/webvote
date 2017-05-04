72414024aefca6d2744400052c9442b729b4a16e
lizusheng <578776370@qq.com>
2017-05-04 10:21:16 +0800

    首页发布投票

---
+++ src/main/resources/static/js/index.js
@@ -0,0 +1,18 @@
+/**
+ * Created by hasee on 2017/5/4.
+ */
+$(document).ready(function(){
+    $(".pagination").createPage({
+        totalPage:10,
+        currPage:1,
+        turndown:'true',
+    });
+});
+
+function changeURL(pageIndex){
+
+}
+
+function change_to_page(pageIndex){
+
+}
