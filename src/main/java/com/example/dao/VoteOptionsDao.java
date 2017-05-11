d75de6ec6930da90094cbadfba3701273905ae90
MyCapitaine <1051943988@qq.com>
2017-05-11 16:17:37 +0800

    修复不能投票的bug 增加了添加投票后的跳转

--- src/main/java/com/example/dao/VoteOptionsDao.java
+++ src/main/java/com/example/dao/VoteOptionsDao.java
@@ -3,7 +3,6 @@ package com.example.dao;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.example.entity.VoteOptionsEntity;
 /**
  * 投票选项DAO
@@ -15,6 +14,5 @@ public interface VoteOptionsDao extends JpaRepository<VoteOptionsEntity, Integer
 	 * 通过投票id查找
 	 */
 	List<VoteOptionsEntity> findByVid(int vid);
-	
 	
 }
